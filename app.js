a5f1ec56d1ae62c255e4b4b22f7591b6ea939cb2
lemonadesquash <ruth.damayanti.sitanggang@gmail.com>
2023-03-09 23:51:50 +0700

    login and register

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
-const express = require('express');
-const router = require('./routes');
+const express = require("express");
+const session = require("express-session");
+const router = require("./routes");
 const app = express();
 const port = 3000;
 
-app.set('view engine','ejs');
+app.use(
+  session({
+    secret: "SECRET_KEY_F_SESSION",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      secure: false,
+      sameSite: true,
+    },
+  })
+);
+
+app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(router);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
--- controllers/controller.js
+++ controllers/controller.js
@@ -1,41 +1,59 @@
+const bcrypt = require("bcryptjs");
+const { User } = require("../models/");
+
 class Controller {
-    static home(req, res) {
-        res.render('home')
+  static home(req, res) {
+    res.render("home");
+  }
+
+  static login(req, res) {
+    const { error } = req.query;
+    res.render("login", { error });
+  }
+
+  static loginPost(req, res) {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      res.send("Wrong email/password");
     }
 
-    static login(req, res) {
-        const {error} = req.query
-        res.render('login', {error})
-    }
+    User.findOne({
+      where: {
+        email,
+      },
+    }).then((foundUser) => {
+      if (!bcrypt.compareSync(password, foundUser.password)) {
+        res.send("Wrong email/password");
+      } else {
+        req.session.user = {
+          id: foundUser.id,
+          username: foundUser.username,
+        };
+        res.redirect(`/prompt/${foundUser.id}`);
+      }
+    });
+  }
 
-    static loginPost(req, res) {
-        const {email, password } = req.body
-        console.log({email, password});
-        
-    }
+  static register(req, res) {
+    res.render("register");
+  }
 
-    static register(req, res) {
-        res.render('register')
-        
-    }
-    
-    static registerPost(req, res) {
-        const {userName, email, password} = req.body
+  static registerPost(req, res) {
+    const { username, email, password } = req.body;
+    User.create({ username, email, password })
+      .then(() => {
+        res.redirect("/login");
+      })
+      .catch((err) => {
+        console.log(err);
+        res.send(err);
+      });
+  }
 
-        User.create({userName, email, password})
-        .then(() => {
-            res.redirect('/login')
-        })
-        .catch(err => {
-            res.send(err)
-        })
-    }
-
-    static courseList(req, res) {
-        res.render('courseList')
-    }
-
-
+  static logout(req, res) {
+    delete req.session.user;
+    res.redirect("/");
+  }
 }
 
-module.exports = Controller
+module.exports = Controller;
